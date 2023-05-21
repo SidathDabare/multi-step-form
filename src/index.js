cb12bb46e04c3edfb1946de03a318404f4ce184f
SidathDabare <sidath2007@gmail.com>
2023-05-22 00:50:02 +0200

    remove redux and unnessary files

--- src/index.js
+++ src/index.js
@@ -4,15 +4,10 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
-import { Provider } from "react-redux"
-import { store, persistor } from "./redux/store"
-import { PersistGate } from "redux-persist/integration/react"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
